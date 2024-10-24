8acfb56fc176ff638d45c5322596df197adac72e
KarinaDAW1 <kizquierdo17331@iesjoanramis.org>
2024-10-24 20:21:52 +0200

    "A medio finalizar. Ejercicio 18"

---
+++ src/EjercicioTema4/Ejercicio18.java
@@ -0,0 +1,26 @@
+package EjercicioTema4;
+
+import java.util.Scanner;
+
+public class Ejercicio18 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int base;
+		int numero;
+		int div;
+		
+		System.out.println("Conversión de numeros enteros en cualquier base n");
+		System.out.println("Escribe el numero \n");
+		
+		numero = sc.nextInt();
+		
+		System.out.println("Escribe la base: \n");
+		
+		base = sc.nextInt();
+		
+		
+	}
+
+}
