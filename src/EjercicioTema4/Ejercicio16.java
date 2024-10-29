de38069c701ee6a49cb2f2d2cb8d27aa24d0db66
KarinaDAW1 <kizquierdo17331@iesjoanramis.org>
2024-10-29 20:02:17 +0100

    Actualizo el Ejercicio 16

--- src/EjercicioTema4/Ejercicio16.java
+++ src/EjercicioTema4/Ejercicio16.java
@@ -6,7 +6,6 @@ public class Ejercicio16 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
 			Scanner sc = new Scanner(System.in);
 			
 	        System.out.print("Introduzca numero de filas: ");
