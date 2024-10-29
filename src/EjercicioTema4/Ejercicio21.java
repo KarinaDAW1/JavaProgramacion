417b5ca07dc5f6e6260944ae9b0debcc003d5809
KarinaDAW1 <kizquierdo17331@iesjoanramis.org>
2024-10-29 21:03:40 +0100

    Ejercicio21 sin acabar.

---
+++ src/EjercicioTema4/Ejercicio21.java
@@ -0,0 +1,21 @@
+package EjercicioTema4;
+
+import java.util.Scanner;
+
+public class Ejercicio21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Introduce una cantidad de euros. (Tiene que ser multiple de 5€)");
+		int cantidad = sc.nextInt();
+		
+		if(cantidad % 5 != 0) {
+			System.out.println("Este número no es multiple de 5, introduce otro número.");
+				
+		}
+		
+	}
+
+}
